2e57dedd4641897e2eb5eaeaaa0f44afadc9c17a
Petr Nymsa <nymsa.petr@outlook.cz>
2025-05-20 11:17:53 +0200

    Add home page drawer

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mdevcamp_workshop/pages/home/filter_cats_button.dart';
+import 'package:mdevcamp_workshop/pages/home/home_page_drawer.dart';
 import 'package:mdevcamp_workshop/pages/home/tabs/adopted/adopted_cats_tab.dart';
 import 'package:mdevcamp_workshop/pages/home/tabs/breeds/breeds_tab.dart';
 import 'package:mdevcamp_workshop/pages/home/tabs/cats/cats_tab.dart';
@@ -20,10 +21,10 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: switch (currentIndex) {
-          0 => const Center(child: Text('Cats')),
-          1 => const Center(child: Text('Breeds')),
-          2 => const Center(child: Text('Adopted')),
-          _ => const Center(child: Text('Unknown')),
+          0 => const Text('Cats'),
+          1 => const Text('Breeds'),
+          2 => const Text('Adopted'),
+          _ => const Text('Unknown'),
         },
         actions: [
           if (currentIndex == 0) const FilterCatsButton(),
@@ -43,6 +44,7 @@ class _HomePageState extends State<HomePage> {
         2 => const AdoptedCatsTab(),
         _ => const Center(child: Text('Unknown')),
       },
+      drawer: const HomePageDrawer(),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: FaIcon(FontAwesomeIcons.cat), label: 'Cats'),
