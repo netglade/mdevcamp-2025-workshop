18d687dc4329b26fa5ddfc4591ac49a0b7cf1235
Petr Nymsa <nymsa.petr@outlook.cz>
2025-05-20 14:34:50 +0200

    Comment out firebase import file

--- bin/firestore_import.dart
+++ bin/firestore_import.dart
@@ -1,41 +1,41 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:mdevcamp_workshop/domain/cat.dart';
-import 'package:mdevcamp_workshop/domain/cat_breed.dart';
-import 'package:mdevcamp_workshop/firebase_options.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart' show rootBundle;
+// import 'package:mdevcamp_workshop/domain/cat.dart';
+// import 'package:mdevcamp_workshop/domain/cat_breed.dart';
+// import 'package:mdevcamp_workshop/firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  // Initialize Firebase using generated options
-  await Firebase.initializeApp(name: 'data-import', options: DefaultFirebaseOptions.android);
-  final firestore = FirebaseFirestore.instance;
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
 
-  // Import breeds
-  final breedsJson = jsonDecode(await rootBundle.loadString('assets/breeds.json')) as List;
-  final breeds = breedsJson
-      .map((e) => CatBreed.fromJson(Map<String, dynamic>.from(e as Map)))
-      .toList();
-  print('Uploading \u001b[33m${breeds.length}\u001b[0m breeds...');
-  for (final breed in breeds) {
-    await firestore.collection('breeds').doc(breed.id).set(breed.toJson());
-  }
-  print('Breeds uploaded.');
+//   // Initialize Firebase using generated options
+//   await Firebase.initializeApp(name: 'data-import', options: DefaultFirebaseOptions.android);
+//   final firestore = FirebaseFirestore.instance;
 
-  // Import cats
-  final catsJson = jsonDecode(await rootBundle.loadString('assets/cats.json')) as List;
-  final cats = catsJson
-      .map((e) => Cat.fromJson(Map<String, dynamic>.from(e as Map)))
-      .toList();
-  print('Uploading \u001b[33m${cats.length}\u001b[0m cats...');
-  for (final cat in cats) {
-    await firestore.collection('cats').doc(cat.id).set(cat.toJson());
-  }
-  print('Cats uploaded.');
+//   // Import breeds
+//   final breedsJson = jsonDecode(await rootBundle.loadString('assets/breeds.json')) as List;
+//   final breeds = breedsJson
+//       .map((e) => CatBreed.fromJson(Map<String, dynamic>.from(e as Map)))
+//       .toList();
+//   print('Uploading \u001b[33m${breeds.length}\u001b[0m breeds...');
+//   for (final breed in breeds) {
+//     await firestore.collection('breeds').doc(breed.id).set(breed.toJson());
+//   }
+//   print('Breeds uploaded.');
 
-  print('Firestore import complete.');
-}
+//   // Import cats
+//   final catsJson = jsonDecode(await rootBundle.loadString('assets/cats.json')) as List;
+//   final cats = catsJson
+//       .map((e) => Cat.fromJson(Map<String, dynamic>.from(e as Map)))
+//       .toList();
+//   print('Uploading \u001b[33m${cats.length}\u001b[0m cats...');
+//   for (final cat in cats) {
+//     await firestore.collection('cats').doc(cat.id).set(cat.toJson());
+//   }
+//   print('Cats uploaded.');
+
+//   print('Firestore import complete.');
+// }
